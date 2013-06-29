5d8c4c470d26efa3c506bbb4a5307fee818f9275
Morgante Pell <morgante.pell@morgante.net>
2013-06-28 22:23:54 -0400

    Update morgante.pp

--- modules/people/manifests/morgante.pp
+++ modules/people/manifests/morgante.pp
@@ -12,7 +12,7 @@ class people::morgante {
   }
 
   # Load dotfiles
-  # $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
 
   repository { $dotfiles_dir:
 	source => "${::github_user}/dotfiles"
