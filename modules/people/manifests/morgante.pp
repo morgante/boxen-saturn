a30fdedb411349145c0dbae8da823aeedd931263
Morgante Pell <morgante.pell@morgante.net>
2013-06-28 22:18:55 -0400

    dotfiles?

--- modules/people/manifests/morgante.pp
+++ modules/people/manifests/morgante.pp
@@ -2,6 +2,31 @@ class people::morgante {
 
   notify { 'class people::morgante declared': }
 
+  # Set my git credentials properly
+  include git
+  git::config::global { 'user.email':
+	value  => 'morgante.pell@morgante.net'
+  }
+  git::config::global { 'user.name' :
+	value  => 'Morgante Pell'
+  }
+
+  # Load dotfiles
+  $home = "/Users/${::boxen_user}"
+  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+
+  repository { $dotfiles_dir:
+	source => "${::github_user}/dotfiles"
+  }
+
+  # Set osx preferences
+  exec { "install dotfiles":
+  	cwd      => $dotfiles_dir,
+  	command  => "./home/.osx",
+  	provider => shell,
+ 	require  => Repository[$dotfiles_dir]
+  }
+
   # Use zsh!
   include zsh
 
